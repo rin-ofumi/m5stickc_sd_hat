3256f6c8c6420a8b3ed44ea6e308a5f9354d68ce
rin_ofumi <57037180+rin-ofumi@users.noreply.github.com>
2023-12-20 13:23:27 +0900

    M5Unified使用に変更、M5StickC Plus2の動作確認。

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # sd_hat
-Arduino project / SD HAT / for M5StickC/Plus
+Arduino project / SD HAT / for M5StickC/Plus/Plus2
 
 <br>
 
 # <概要>
 
-M5StickC/Plus用「SD HAT」のサンプルプログラムです。（[「SD HAT」はBOOTHにて販売中](https://kitto-yakudatsu.booth.pm/items/2385035)）<br>
+M5StickC/Plus/Plus2用「SD HAT」のサンプルプログラムです。（[「SD HAT」はBOOTHにて販売中](https://kitto-yakudatsu.booth.pm/items/2385035)）<br>
 
 <br>
 
@@ -13,13 +13,19 @@ M5StickC/Plus用「SD HAT」のサンプルプログラムです。（[「SD HAT
 
 ## 「sd_test.ino」
 SDカードの種類・容量・ファイル一覧を表示するArduinoスケッチです。<br>
-M5StickC/Plusどちらにも使えます。※includeコメントアウト差し替えが必要です
+M5StickC/Plus/Plus2で使用出来ます。※Arduino IDEのボードマネージャーで各モデルを指定して下さい。
 
 <br>
 
 # <アップデート履歴>
 
+## 【2023.12.20】
+
+* M5Unified使用に変更、M5StickC Plus2の動作確認。
+
+<br>
+
 ## 【2020.9.19】
 
-* 最初のリリース
+* 最初のリリース。
 
--- sd_test.ino
+++ sd_test.ino
@@ -1,8 +1,24 @@
-//#include <M5StickC.h> //M5StickCの場合はコチラをアクティブにする
-#include <M5StickCPlus.h> //M5StickCPlusの場合はコチラをアクティブにする
-#include "SD.h"
+// M5StickC/Plus/Plus2用「SD HAT」のサンプルプログラム
+// @rin_ofumi 2023.12.20
+//
+// 確認した機種
+// - M5StickC
+// - M5StickC Plus
+// - M5StickC Plus2
+//
+// 開発環境
+// - ArduinoIDE @^2.2.1
+//
+// ボードマネージャバージョン
+// - M5Stack @^2.0.9
+//
+// 使用ライブラリバージョン
+// - M5Unified @^0.1.11
 
-SPIClass SPI2;
+#include <SD.h>
+#include <M5Unified.h>
+
+SPIClass SPI_EXT;
 
 void setup() {
   M5.begin();
@@ -11,10 +27,10 @@ void setup() {
   M5.Lcd.setRotation(1);
 
   //SDカード用SPIポート初期化（SCK:HAT_G0, MISO:HAT_G36, MOSI:HAT_G26, SS:物理ピン無し"-1"）
-  SPI2.begin(0, 36, 26, -1);
+  SPI_EXT.begin(0, 36, 26, -1);
 
   //SDカード初期設定
-  if (!SD.begin(-1, SPI2)) {
+  if (!SD.begin(-1, SPI_EXT, 15000000)) {
     M5.Lcd.println("SD Card Mount Failed!");
     return;
   }
